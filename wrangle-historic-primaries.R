e3b89cab41ffb8b15461f40112f0ff031d3f98e5
Rob Lytle <47059344+RobLytle@users.noreply.github.com>
2021-10-12 12:56:41 -0400

    getting old primaries in

--- wrangle-historic-primaries.R
+++ wrangle-historic-primaries.R
@@ -1,4 +1,29 @@
 library(tidyverse)
+library(anesr)
+data(timeseries_1980) #these import the datasets from anesr
+data(timeseries_1988)
+data(timeseries_1992)
+data(timeseries_cum)
+
+df_cdf <- timeseries_cum
+df_80<-timeseries_1980
+df_88<-timeseries_1988
+df_92<-timeseries_1992
+
+df_tid_80 <- df_80 %>%
+	select(case = V800004,
+				 V800371:V800373,
+				 therm_dem = V800168,
+				 therm_rep = V800169) %>% 
+	glimpse()
+
+
+
+df_tid_92 <- df_92 %>% 
+select(V92301:V92304,
+				therm_dem = V923317,
+				therm_rep = V923318) %>% 
+	glimpse()
 
 #Going to wrangle all remaining years with primary info
 
@@ -28,3 +53,8 @@ library(tidyverse)
 # Primary Votes: 800371-800373
 # Therms: D-800168
 #					R-800169
+
+tidy_cdf <- read_rds("data/tidy-cdf.rds") %>%
+	filter(year == 1980) %>% 
+	select(case) %>% 
+	glimpse()
